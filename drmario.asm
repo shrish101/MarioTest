9ff995339a4ae6c969aa6524aac00ed9d5b22cd1
Ujivl <90654578+Ujivl@users.noreply.github.com>
2025-03-19 17:02:25 -0400

    added a basic game loop (with syscall sleep) with keyboard input tracking for the key Q, also made color a little bit more efficient

--- drmario.asm
+++ drmario.asm
@@ -39,72 +39,68 @@ ADDR_KBRD:
 li $t1, 0xff0000 # $t1 = red
 li $t2, 0x00ff00 # $t2 = green
 li $t3, 0x0000ff # $t3 = blue
-li $t9, 0xffffff        # white (we should use this as the default and have an option in the functions where u can change color
 lw $t0, ADDR_DSPL
+lw $t8, ADDR_KBRD
 
-# a0 = 10, a1 = 15, t6 = 30, a2/orientation = horizontal, color/t9 = white
+# a0 = 10, a1 = 15, t6 = 30, a2/orientation = horizontal, color/a3 = white
 line_one:
-add $a0, $zero, 10   #x-axis
-add $a1, $zero, 15   #y-axis
-add $t6, $zero, 30   #size
-add $t9, $zero, $t9  #color
-li $a2, 1            #flag for horizontal or vertical
+add $a0, $zero, 10          #x-axis
+add $a1, $zero, 15          #y-axis
+add $t6, $zero, 30          #size
+add $a3, $zero, 0xffffff    #color
+li $a2, 1                   #flag for horizontal or vertical
 li $t4, 0
 jal draw_line
-li $t9, 0xffffff
 
-# x/a0 = 10, y/a1 = 15, size/t6 = 40, a2/orientation = vertical, color/t9 = white
+# x/a0 = 10, y/a1 = 15, size/t6 = 40, a2/orientation = vertical, color/a3 = white
 line_two:
-add $a0, $zero, 10   #x-axis
-add $a1, $zero, 15   #y-axis
-add $t6, $zero, 40   #size
-add $t9, $zero, $t9  #color
-li $a2, 0            #flag for horizontal or vertical
+add $a0, $zero, 10          #x-axis
+add $a1, $zero, 15          #y-axis
+add $t6, $zero, 40          #size
+add $a3, $zero, 0xffffff    #color
+li $a2, 0                   #flag for horizontal or vertical
 li $t4, 0
 jal draw_line
-li $t9, 0xffffff
 
-# x/a0 = 10, y/a1 = 15, size/t6 = 40, a2/orientation = vertical, color/t9 = white
+# x/a0 = 10, y/a1 = 15, size/t6 = 40, a2/orientation = vertical, color/a3 = white
 line_three:
-add $a0, $zero, 40   #x-axis
-add $a1, $zero, 15   #y-axis
-add $t6, $zero, 41   #size
-add $t9, $zero, $t9  #color
-li $a2, 0            #flag for horizontal or vertical
+add $a0, $zero, 40          #x-axis
+add $a1, $zero, 15          #y-axis
+add $t6, $zero, 41          #size
+add $a3, $zero, 0xffffff    #color
+li $a2, 0                   #flag for horizontal or vertical
 li $t4, 0
 jal draw_line
 li $t9, 0xffffff
 
-# x/a0 = 10, y/a1 = 55, size/t6 = 30, a2/orientation = horizontal, color/t9 = white
+# x/a0 = 10, y/a1 = 55, size/t6 = 30, a2/orientation = horizontal, color/a3 = white
 line_four:
-add $a0, $zero, 10   #x-axis
-add $a1, $zero, 55   #y-axis
-add $t6, $zero, 30   #size
-add $t9, $zero, $t9  #color
-li $a2, 1            #flag for horizontal or vertical
+add $a0, $zero, 10          #x-axis
+add $a1, $zero, 55          #y-axis
+add $t6, $zero, 30          #size
+add $a3, $zero, 0xffffff    #color
+li $a2, 1                   #flag for horizontal or vertical
 li $t4, 0
 jal draw_line
-li $t9, 0xffffff
 
-# x/a0 = 10, y/a1 = 15, size/t6 = 40, a2/orientation = vertical, color/t9 = white
+# x/a0 = 10, y/a1 = 15, size/t6 = 40, a2/orientation = vertical, color/a3 = black
 line_five:
-add $a0, $zero, 20   #x-axis
-add $a1, $zero, 15  #y-axis
-add $t6, $zero, 10  #size
-add $t9, $zero, 0x000000  #color
-li $a2, 1          #flag for horizontal or vertical
+add $a0, $zero, 20          #x-axis
+add $a1, $zero, 15          #y-axis
+add $t6, $zero, 10          #size
+add $a3, $zero, 0x000000    #color
+li $a2, 1                   #flag for horizontal or vertical
 li $t4, 0
 jal draw_line
-li $t9, 0xffffff
 
-j exit
+j game_update_loop  # after drawing everything program counter goes to the game loop
 
 draw_line:
     sll $t5, $a1, 6      # shifts a1 by 6 bits and stores it in t5, equivalent to y * 64
     add $t5, $t5, $a0    # add x offset to t5 to get pixel position
     sll $t5, $t5, 2      # multiply by 4 (word size)
     add $t5, $t5, $t0    # add base address
-    sw $t9, 0($t5)       # store color
+    sw $a3, 0($t5)       # store color
     
     beq $a2, 1, move_horizontal 
     beq $a2, 0, move_vertical 
@@ -125,4 +121,19 @@ move_vertical:
 return_to_caller:
     jr $ra
 
+game_update_loop:
+    lw $t9, 0($t8)                  # Load first word from keyboard
+	
+	# Sleep for 17 ms so frame rate is about 60
+	addi	$v0, $zero, 32	# syscall sleep
+	addi	$a0, $zero, 17	# 17 ms
+	syscall
+	    
+	bne $t9, 1, game_update_loop      # If t9 == 1, or if a key is pressed, move on to next instruct, else loop
+	lw $t9, 4($t8)                    # load the second word into $t9
+    bne $t9, 0x71, game_update_loop   # If t9 = (user inputs Q), move on to the next instruct, else loop
+    j exit                            # this will only happen if the user presses Q
+
 exit:
+	li $v0, 10                      # Quit gracefully
+	syscall
