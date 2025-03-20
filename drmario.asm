2ec0e66f028a14fd97f1bc5736fd9e5a871abdcf
Ujivl <90654578+Ujivl@users.noreply.github.com>
2025-03-19 20:28:52 -0400

    movement for capsule

--- drmario.asm
+++ drmario.asm
@@ -93,7 +93,7 @@ li $a2, 1                   #flag for horizontal or vertical
 li $t4, 0
 jal draw_line
 
-j game_update_loop  # after drawing everything program counter goes to the game loop
+j pill_coords  # after drawing everything program counter goes to the game loop
 
 draw_line:
     sll $t5, $a1, 6      # shifts a1 by 6 bits and stores it in t5, equivalent to y * 64
@@ -121,19 +121,45 @@ move_vertical:
 return_to_caller:
     jr $ra
 
+pill_coords:
+    li $a1, 25  # x
+    li $a2, 15   # y
+    jal pill_set_pos
+
 game_update_loop:
     lw $t9, 0($t8)                  # Load first word from keyboard
 	
 	# Sleep for 17 ms so frame rate is about 60
 	addi	$v0, $zero, 32	# syscall sleep
-	addi	$a0, $zero, 17	# 17 ms
+	addi	$a0, $zero, 66	# 17 ms
 	syscall
-	    
-	bne $t9, 1, game_update_loop      # If t9 == 1, or if a key is pressed, move on to next instruct, else loop
+	
+	add $a3, $zero, 0x000000    #color
+	jal pill_set_pos
+	addi $a2, $a2, 1
+	
+	bne $t9, 1, else      # If t9 == 1, or if a key is pressed, move on to next instruct, else loop
 	lw $t9, 4($t8)                    # load the second word into $t9
-    bne $t9, 0x71, game_update_loop   # If t9 = (user inputs Q), move on to the next instruct, else loop
-    j exit                            # this will only happen if the user presses Q
+    bne $t9, 0x61, elif               # If t9 = (user inputs A), move on to the next instruct, else loop. (pretend its not Q rn)
+    addi $a1, $a1, -1
+elif: bne $t9, 0x64, else
+    addi $a1, $a1, 1
+else: 
+    add $a3, $zero, 0xffffff
+    jal pill_set_pos
+    
+    # here we can implement a reset for the pill location
+    
+    j game_update_loop
 
 exit:
 	li $v0, 10                      # Quit gracefully
 	syscall
+    
+pill_set_pos:
+    sll $t5, $a2, 6      # shifts a1 by 6 bits and stores it in t5, equivalent to y * 64
+    add $t5, $t5, $a1    # add x offset to t5 to get pixel position
+    sll $t5, $t5, 2      # multiply by 4 (word size)
+    add $t5, $t5, $t0    # add base address
+    sw $a3, 0($t5)       # store color
+    jr $ra
